572c24299950f2652c8b69c3188537edf009049f
nipunar99 <2020cs019@stu.ucsc.cmb.ac.lk>
2022-07-09 12:45:11 +0530

    answers for all 1 and 4 questions

---
+++ 1.scala
@@ -0,0 +1,17 @@
+import scala.compiletime.ops.float
+object main extends App{
+    var i, j, k = 2;
+    var m, n = 5;
+    var f:Float = 12.0f;
+    var g:Float = 4.0f;
+    var c='X';
+
+    println("(a) k + 12 * m ="  +  (k + 12*m));
+    println("(a) m/j ="  +  (m/j));
+    println("(a) n%j ="  +  (n%j));
+    println("(a) m/j*j ="  +  (m/j*j));
+    println("(a) f + 10*5 + g ="  +  (f + 10*5 + g));
+    //println("(a) ++i*n"  +  (++i*n));
+
+
+}
---
+++ 4a.scala
@@ -0,0 +1,9 @@
+object main extends App{
+    def normalHourSalary(h :Int):Int =250*h;
+    def OTsalary(h :Int):Int = h*85;
+    def totaSalary(nh :Int, oth: Int):Int = normalHourSalary(nh) + OTsalary(oth);
+    def taxAmount(nh :Int, oth: Int):Float = totaSalary(nh,oth)*12/100;
+    def takeHomeSalary(nh :Int,oth :Int):Float = totaSalary(nh,oth)-taxAmount(nh,oth);
+
+    println(takeHomeSalary(40,30));
+}
