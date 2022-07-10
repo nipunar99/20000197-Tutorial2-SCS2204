103b199c5e75fef6a24aceb04c95a3bb6a4f531d
nipunar99 <2020cs019@stu.ucsc.cmb.ac.lk>
2022-07-10 20:39:00 +0530

    Final

--- Question 4a.scala
+++ Question 4a.scala
@@ -2,8 +2,8 @@ object main extends App{
     def normalHourSalary(h :Int):Int =250*h;
     def OTsalary(h :Int):Int = h*85;
     def totaSalary(nh :Int, oth: Int):Int = normalHourSalary(nh) + OTsalary(oth);  //nh: normal hours | oth: OT hours
-    def taxAmount(nh :Int, oth: Int):Float = totaSalary(nh,oth)*12/100;
+    def taxAmount(nh :Int, oth: Int) = totaSalary(nh,oth)*12/100.toFloat;
     def takeHomeSalary(nh :Int,oth :Int):Float = totaSalary(nh,oth)-taxAmount(nh,oth);
 
-    println(takeHomeSalary(40,30));
+    println("Take Home Salary of a typical employee: " + takeHomeSalary(40,30));
 }
