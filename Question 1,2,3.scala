d637c925dd8cf2483f05e246c6b978afff1d3317
nipunar99 <2020cs019@stu.ucsc.cmb.ac.lk>
2022-07-10 21:36:05 +0530

    Final Final

--- Question 1,2,3.scala
+++ Question 1,2,3.scala
@@ -3,8 +3,7 @@ object main extends App{
 
     println("PART 1");
     //part 1
-    var i, j=2;
-    var k: Float = 2;   //here I defined k as float because later in the 
+    var i, j, k = 2;   
     var m, n = 5;
     var f:Float = 12.0f;
     var g:Float = 4.0f;
@@ -15,27 +14,9 @@ object main extends App{
     println("(c) n%j ="  +  (n%j));
     println("(d) m/j*j ="  +  (m/j*j));
     println("(e) f + 10*5 + g ="  +  (f + 10*5 + g));
-    //println("(a) ++i*n"  +  (++i*n));
+    //println("(f) ++i*n"  +  (++i*n));
 
     println();
-    println("PART 2");
-
-    //part 2
-    var a = 2;
-    var b = 3;
-    c = 4;
-    var d = 5;
-    k= 4.3;
-
-    //println(--b*a+c*d--);
-    //println(a++);
-    println("(c) " + (-2*(g-k)+c));
-    //println(c=c++);
-    //println(c=++c*a++);
-
-    /*these Commented codes cannotbe evaluated because in scala 
-    incremental and decremental operations such as ++ and -- are 
-    considered to be invalid. Only (c)can be evaluated*/
-
+   
 
 }
---
+++ Question 3part2.scala
@@ -0,0 +1,23 @@
+
+object main extends App{
+    println("PART 2");
+
+    //part 2
+    var a = 2;
+    var b = 3;
+    var c = 4;
+    var d = 5;
+    var k = 4.3;
+    var g = 4.0;
+
+    //println(--b*a+c*d--);
+    //println(a++);
+    println("(c) " + (-2*(g-k)+c));
+    //println(c=c++);
+    //println(c=++c*a++);
+
+    /*these Commented codes cannotbe evaluated because in scala 
+    incremental and decremental operations such as ++ and -- are 
+    considered to be invalid. Only (c)can be evaluated*/
+
+}
